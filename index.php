ffb6413f3ac676234dbd725e0ba948800acfcaf5
elger <elgervb@gmail.com>
2015-04-29 23:04:35 +0200

    add the app directory to the classpath

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 use compact\mvvm\FrontController;
 include_once '../compact/classes/compact/ClassLoader.php';
-compact\ClassLoader::create();
+compact\ClassLoader::create()->addClassPath(__DIR__."/app");
 
 $fc = new FrontController();
 $fc->run();
