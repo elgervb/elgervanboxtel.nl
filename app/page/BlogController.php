e0ff6590a16d34f3d2866367c2adffa5fad6bacb
elger <elgervb@gmail.com>
2015-05-11 23:20:46 +0200

    Blog: set page title according to post #10
    closes #10

--- app/page/BlogController.php
+++ app/page/BlogController.php
@@ -4,26 +4,35 @@ namespace app\page;
 use compact\mvvm\impl\ViewModel;
 use compact\Context;
 use classes\NestedView;
+
 class BlogController 
 {
     /**
-     * @return \core\mvc\impl\view\ViewModel
+     * Show a list of posts on screen
+     * 
+     * @return \classes\NestedView The layout
      */
     public function index(){
 
         return new NestedView(
-            new ViewModel('blog/layout/index.html'),
+            new ViewModel('blog/layout/index.html', array('pagetitle'=>'Elger van Boxtel Personal Blog')),
             new ViewModel('blog/layout/header.html'),  
             new ViewModel('blog/index.html') 
         );
     }
     
-    public function item($item){
+    /**
+     * Show a blog post on screen
+     * 
+     * @param string $item the title of the post. This must resemble the filename in /view/blog/items/$item.html
+     * 
+     * @return \classes\NestedView The layout
+     */
+    public function post($item){
         return new NestedView(
-            new ViewModel('blog/layout/index.html'),
+            new ViewModel('blog/layout/index.html', array('pagetitle'=>ucfirst(strtolower(str_replace('-', ' ', $item))))),
             new ViewModel('blog/layout/header.html'),
             new ViewModel('blog/items/'.$item.'.html')
         );
-        
     }
 }
